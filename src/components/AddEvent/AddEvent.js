cfa241f281ebe7858beb403873811fc0ab650866
WreetSarker <wreet.sarker@gmail.com>
2021-03-29 13:18:42 +0600

    added post method to send data to database

--- src/components/AddEvent/AddEvent.js
+++ src/components/AddEvent/AddEvent.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 
 const AddEvent = () => {
     const { register, handleSubmit, watch, errors } = useForm();
-    const onSubmit = data => console.log(data);
+    const [imageURL, setImageURL] = useState(null);
+
+    const onSubmit = data => {
+        const eventData = {
+            name: data.name,
+            imageURL: imageURL,
+        }
+
+
+        fetch('http://localhost:4000/addEvent', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(eventData)
+        })
+            .then(resp => console.log('server resp', resp))
+    }
+
+    const handleImageUpload = event => {
+        // console.log(event.target.files[0]);
+        const imageData = new FormData();
+        imageData.set('key', '3dacd4bde4736caf7263e39e48f3a316');
+        imageData.append('image', event.target.files[0]);
+
+        axios.post('https://api.imgbb.com/1/upload',
+            imageData)
+            .then(async function (response) {
+                await setImageURL(response.data.data.display_url);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
     return (
         <div>
             <h1>Add your events here</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input name="example" defaultValue="test" ref={register} />
+                <input name="name" defaultValue="New Event" ref={register} />
                 <br />
-                <input name="exampleRequired" ref={register({ required: true })} />
+                <input name="exampleRequired" type="file" onChange={handleImageUpload} />
                 <br />
                 <input type="submit" />
             </form>
