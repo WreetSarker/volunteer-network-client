49409ef4711eec887733a1f6f0c85c1033d5af4b
WreetSarker <wreet.sarker@gmail.com>
2021-03-30 11:19:52 +0600

    changed registration field and removed firebase authentication from it

--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -1,15 +1,14 @@
+import 'date-fns';
 import React, { useContext, useState } from 'react';
-import firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from './firebase.config';
 import { useHistory, useLocation } from 'react-router';
 import { UserContext } from '../../App';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-} else {
-    firebase.app();
-}
 
 const Register = () => {
     const history = useHistory();
@@ -31,62 +30,24 @@ const Register = () => {
         success: false,
     });
 
+    const [selectedDate, setSelectedDate] = useState({
+        date: new Date()
+    })
+
+    const handleDateChange = (date) => {
+        const newDate = { ...selectedDate };
+        newDate.date = date
+        setSelectedDate(newDate);
+    };
+
 
     const handleSubmit = (e) => {
-        if (newUser && user.email && user.password) {
-            firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
-                .then((res) => {
-                    const newUserInfo = { ...user };
-                    newUserInfo.error = '';
-                    newUserInfo.success = true;
-                    setUser(newUserInfo);
-                    updateUserName(user.name);
-                    setLoggedInUser(newUserInfo)
-                    history.replace(from)
 
-                })
-                .catch((error) => {
-                    const newUserInfo = { ...user };
-                    newUserInfo.error = error.message;
-                    newUserInfo.success = false;
-                    setUser(newUserInfo)
-                });
-        }
-        if (!newUser && user.email && user.password) {
-            firebase.auth().signInWithEmailAndPassword(user.email, user.password)
-                .then((res) => {
-                    if (pass.password === pass.confirmPassword) {
-                        const newUserInfo = { ...user };
-                        newUserInfo.error = '';
-                        newUserInfo.success = true;
-                        setUser(newUserInfo);
-                        setLoggedInUser(newUserInfo);
-                        history.replace(from)
-                    }
-                })
-                .catch((error) => {
-                    const newUserInfo = { ...user };
-                    newUserInfo.error = error.message;
-                    newUserInfo.success = false;
-                    setUser(newUserInfo)
-                });
-        }
+
 
         e.preventDefault();
     }
 
-    const updateUserName = name => {
-        const user = firebase.auth().currentUser;
-
-        user.updateProfile({
-            displayName: name,
-
-        }).then(function () {
-            // Update successful.
-        }).catch(function (error) {
-            // An error happened.
-        });
-    }
 
 
     const handleBlur = (event) => {
@@ -96,26 +57,7 @@ const Register = () => {
         if (event.target.name === 'email') {
             isFieldValid = /\S+@\S+\.\S+/.test(event.target.value);
         }
-        if (event.target.name === 'password') {
-            const isPasswordValid = event.target.value.length > 6;
-            const passwordHasNumber = /\d{1}/.test(event.target.value);
-            isFieldValid = isPasswordValid && passwordHasNumber;
-            if (isFieldValid) {
-                const newPassInfo = { ...pass };
-                newPassInfo[event.target.name] = event.target.value;
-                setPass(newPassInfo)
-            }
-        }
-        if (event.target.name === 'confirmPassword') {
-            const isConfirmedPasswordValid = event.target.value.length > 6;
-            const confirmedPasswordHasNumber = /\d{1}/.test(event.target.value);
-            isFieldValid = isConfirmedPasswordValid && confirmedPasswordHasNumber;
-            if (isFieldValid) {
-                const newPassInfo = { ...pass };
-                newPassInfo[event.target.name] = event.target.value;
-                setPass(newPassInfo)
-            }
-        }
+
 
         if (isFieldValid) {
             const newUserInfo = { ...user };
@@ -125,22 +67,34 @@ const Register = () => {
     }
     return (
         <div>
-            <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id="" />
-
-            <label htmlFor="newUser">New User Sign up</label>
             <form onSubmit={handleSubmit}>
-                {newUser && <input type="text" onBlur={handleBlur} name="name" placeholder="type your name" />}
+                <input type="text" onBlur={handleBlur} name="name" placeholder="Full Name" />
                 <br />
-                <input type="text" onBlur={handleBlur} name="email" placeholder="email" required />
+                <input type="text" onBlur={handleBlur} name="email" placeholder="Email" required />
                 <br />
-                <input type="password" onBlur={handleBlur} name="password" placeholder="password" required />
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <Grid container justify="start">
+                        <KeyboardDatePicker
+                            disableToolbar
+                            variant="inline"
+                            format="dd/MM/yyyy"
+                            margin="normal"
+                            id="date-picker-inline"
+                            label="Date"
+                            value={selectedDate.date}
+                            onChange={handleDateChange}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}
+                        />
+                    </Grid>
+                </MuiPickersUtilsProvider>
                 <br />
-                <input type="password" onBlur={handleBlur} name="confirmPassword" placeholder="confirm password" required />
+                <input type="text" onBlur={handleBlur} name="eventName" placeholder="Event Name" required />
                 <br />
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Registration" />
             </form>
-            <p style={{ color: 'red' }}>{user.error}</p>
-            {user.success && console.log('success') && <h2 style={{ color: 'red' }}>User {newUser ? 'created' : 'logged in'} successfully</h2>}
+
         </div>
     );
 };
